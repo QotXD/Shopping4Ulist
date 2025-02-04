3be9a667971f8385feb7a49d714dc9063e57f404
Simo Karhu <94228072+QotXD@users.noreply.github.com>
2025-02-04 12:17:14 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -32,11 +32,10 @@ function clearList() {
 }
 var GHPATH = '/Shopping4Ulist';
 var APP_PREFIX = 's4ul_';
-var VERSION = 'version_00';
+var VERSION = 'version_02';
 var URLS = [    
     `${GHPATH}/`,
     `${GHPATH}/index.html`,
     `${GHPATH}/styles.css`,
     `${GHPATH}/app.js`
-    `${GHPATH}/icon.png`
   ]
